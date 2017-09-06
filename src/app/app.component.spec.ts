b0e620e69762ed3cc08f63241bc8f51a82fa418d
Juan Herrera <david.juanherrera@gmail.com>
2017-09-06 16:22:20 -0500

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,19 +1,12 @@
-// 2. Create a basic testing structure using what you think it's necesary
-describe('AppComponent', ()=>{
+// 1. Create a basic testing structure using what you think it's necesary
+describe('AppComponent', () => {
 
 	// 2. declare a boolean variable called truly 
-	let truly;
-
-	beforeEach(()=>{
-		// NOTE: truly variable will be reassigned to true before each spec
-		truly = true;
-	});
-
-	// 3. Evaluate truly variable, be sure it's value is true
-	it('should evaluate truly variable to return true', ()=>{
-		// it's ok this way
-		// expect(truly).toBeTruthy();
-		// but you can do it like this too
+	let truly = true;
+  
+	// 3. Evaluate truly variable, be sure its value is true
+	it('should evaluate truly variable to return true', () => {
 		expect(truly).toBe(true);
+    // or expect(truly).toBeTruthy();
 	});
 })
