cde4ba6a5ce6878b874d64e08dce2c6865ea4b8f
Juan Esteban Villa <juan.es-19@hotmail.com>
2017-09-07 07:53:42 -0500

    README.md

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,22 +4,15 @@ import { AppComponent } from "./app.component";
 describe('AppComponent', ()=>{
 	// 2. declare a new instance of AppComponent class 	
 	let component = new AppComponent();
-
-	// 3. Create a string variable title
-	let title: string = 'Chuck Norris Jokes';
-	let truly;
-
-	beforeEach(()=>{
-		truly = true;
-	});
+	let truly: boolean = true;
 
 	it('should evaluate truly variable to return true', ()=>{
 		// expect(truly).toBeTruthy();
 		expect(truly).toBe(true);
 	});
 
-	// 4. spec to evaluate title of AppComponent class
+	// 3. spec to evaluate title of AppComponent class
 	it('should have as title "Chuck Norris Jokes"', ()=>{
-		expect(component.title).toEqual(title);
+		expect(component.title).toEqual('Chuck Norris Jokes');
 	});
 })
