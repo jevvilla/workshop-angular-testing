8087b745d384effbcae4f2e9a2140566385bb867
Juan Esteban Villa <juan.es-19@hotmail.com>
2017-09-08 06:46:53 -0500

    README.md

--- src/app/capitalize.pipe.ts
+++ src/app/capitalize.pipe.ts
@@ -7,9 +7,8 @@ export class CapitalizePipe implements PipeTransform {
 
   transform(value: string): string {
     if(!value) return;
-    return value.toLowerCase().split(" ").reduce((acum, curr) => {
-      acum += `${curr[0].toUpperCase()}${curr.substr(1)} `;
-      return acum; 
-    }, "")
+    return (value.toLowerCase().split(" ").reduce((acum, curr) => {
+      return acum += `${curr[0].toUpperCase()}${curr.substr(1)} `;
+    }, "")).trim();
   }
 }
