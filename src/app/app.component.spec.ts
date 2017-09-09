2f9410d82a646391d39be809f685d81ef17ba740
jdjuan <david.juanherrera@gmail.com>
2017-09-09 07:00:51 -0500

    refactor: fix format and indentation

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,18 @@
 // 1. import what you think it's necessary
-import { AppComponent } from "./app.component";
+import { AppComponent } from './app.component';
 
-describe('AppComponent', ()=>{
-	// 2. declare a new instance of AppComponent class 	
-	let component = new AppComponent();
-	let truly: boolean = true;
+describe('AppComponent', () => {
+  // 2. declare a new instance of AppComponent class
+  const component = new AppComponent();
+  const truly = true;
 
-	it('should evaluate truly variable to return true', ()=>{
-		// expect(truly).toBeTruthy();
-		expect(truly).toBe(true);
-	});
+  it('should evaluate truly variable to return true', () => {
+    // expect(truly).toBeTruthy();
+    expect(truly).toBe(true);
+  });
 
-	// 3. spec to evaluate title of AppComponent class
-	it('should have as title "Chuck Norris Jokes"', ()=>{
-		expect(component.title).toEqual('Chuck Norris Jokes');
-	});
-})
+  // 3. spec to evaluate title of AppComponent class
+  it('should have as title "Chuck Norris Jokes"', () => {
+    expect(component.title).toEqual('Chuck Norris Jokes');
+  });
+});
