79f5d79232db8e29240ab7a38366e9be915c0ace
jdjuan <david.juanherrera@gmail.com>
2017-09-09 07:01:56 -0500

    refactor: fix indentation

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,12 @@
 // 1. Create a basic testing structure using what you think it's necessary
 describe('AppComponent', () => {
 
-	// 2. declare a boolean variable called truly 
-	let truly = true;
-  
-	// 3. Evaluate truly variable, be sure its value is true
-	it('should evaluate truly variable to return true', () => {
-		expect(truly).toBe(true);
+  // 2. declare a boolean variable called truly
+  const truly = true;
+
+  // 3. Evaluate truly variable, be sure its value is true
+  it('should evaluate truly variable to return true', () => {
+    expect(truly).toBe(true);
     // or expect(truly).toBeTruthy();
-	});
-})
+  });
+});
